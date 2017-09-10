84b475c7ed2b3163ff97bfcf2aae2f5dedc83ec4
Louis Rubet <louis@rubet.fr>
2017-09-10 12:19:46 +0200

    #90: debugged faulty script

---
+++ db/#87-add-lieu-actuel-in-list-view.sql
@@ -0,0 +1,18 @@
+USE `workshapedb`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `workshape`@`%` 
+    SQL SECURITY DEFINER
+VIEW `product_list` AS
+    SELECT 
+        `product`.`qr_code` AS `qr_code`,
+        COALESCE(`product`.`name`, `product`.`qr_code`) AS `name`,
+        COALESCE(`product`.`longueur_actuelle`,
+                `product`.`longueur_initiale`) AS `longueur_actuelle`,
+        `product`.`lieu_actuel` AS `lieu_actuel`
+    FROM
+        `product`
+    WHERE
+        (ISNULL(`product`.`finished`)
+            OR (`product`.`finished` <> 1))
+    ORDER BY `product`.`date_creation` DESC;
